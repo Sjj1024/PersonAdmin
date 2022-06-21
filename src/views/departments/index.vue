57b8380a792baaa55e89de8d988da523080d8eb7
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2022-06-21 09:49:48 +0800

    完成新增部门的规则校验

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -8,10 +8,13 @@
             slot-scope="{ data }"
             :tree-node="data"
             @delDepts="getDepartments"
+            @addDepts="addDepts"
           />
         </el-tree>
       </el-card>
     </div>
+    <!-- 放置新增弹层组件  -->
+    <add-dept :show-dialog="showDialog" :tree-node="node" />
   </div>
 </template>
 
@@ -19,9 +22,12 @@
 import TreeTools from "./components/tree-tools";
 import { getDepartments } from "@/api/departments";
 import { tranListToTreeData } from "@/utils/index";
+import AddDept from "./components/add-dept"; // 引入新增部门组件
+
 export default {
   components: {
     TreeTools,
+    AddDept,
   },
   data() {
     return {
@@ -30,6 +36,8 @@ export default {
       defaultProps: {
         label: "name", // 表示 从这个属性显示内容
       },
+      showDialog: false, // 显示窗体,
+      node: null, // 操作的节点
     };
   },
   created() {
@@ -38,9 +46,15 @@ export default {
   methods: {
     async getDepartments() {
       const result = await getDepartments();
-      this.company = { name: result.companyName, manager: "负责人" };
+      this.company = { name: result.companyName, manager: "负责人", id: "" };
       // 这里定义一个空串  因为 它是根 所有的子节点的数据pid 都是 ""
       this.departs = tranListToTreeData(result.depts, "");
+    },
+    //增加方法
+    addDepts(node) {
+      this.showDialog = true; // 显示弹层
+      // 因为node是当前的点击的部门， 此时这个部门应该记录下来,
+      this.node = node;
     },
   },
 };
